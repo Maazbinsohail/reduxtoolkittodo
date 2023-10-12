f99977caa7708a1e9b971dbedf6d22c5db396858
Maazbinsohail <maazbinsohail15@gmail.com>
2023-10-12 09:39:58 -0700

    still in process

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Todo with redux tool  kit </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimpleTodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SimpleTodo() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SimpleTodo
