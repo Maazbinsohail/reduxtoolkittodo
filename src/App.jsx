4498f50af1d5245d3fe3804bb9888cdf5751e20c
Maazbinsohail <Maazbinsohail15@gmail.com>
2023-10-13 12:23:49 -0700

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
-import './App.css'
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import SimpleTodo from "./components/SimpleTodo";
 
 function App() {
-
-
   return (
     <>
-    <h1>Todo with redux tool  kit </h1>
+      <h1 className="text-3xl text-white hover:underline capitalize ">Todo with redux tool kit </h1>
+      <AddTodo />
+      <SimpleTodo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
